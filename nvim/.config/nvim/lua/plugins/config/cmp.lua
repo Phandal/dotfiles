99912d1b66c2e857292e4113d3800dc076a14a4a
Phandal <bfeld.111@gmail.com>
2025-01-09 08:45:49 -0600

    changed cmp setup

--- nvim/.config/nvim/lua/plugins/config/cmp.lua
+++ nvim/.config/nvim/lua/plugins/config/cmp.lua
@@ -4,55 +4,55 @@ local lspkind = require("lspkind")
 local luasnip = require("luasnip")
 require("luasnip.loaders.from_vscode").lazy_load()
 
--- cmp.setup({
---   -- window = {
---   --   completion = {
---   --     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
---   --   },
---   --   documentation = {
---   --     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
---   --   }
---   -- },
---   formatting = { -- This is for lspkind
---     format = lspkind.cmp_format({
---       mode = 'symbol_text',
---       maxwidth = 50,
---     })
---   },
---   snippet = {
---     expand = function(args)
---       luasnip.lsp_expand(args.body)
---     end,
---   },
---   mapping = cmp.mapping.preset.insert({
---     ["<C-p>"] = cmp.mapping.select_prev_item(),
---     ["<C-n>"] = cmp.mapping.select_next_item(),
---     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
---     ["<C-f>"] = cmp.mapping.scroll_docs(4),
---     ["<C-Space>"] = cmp.mapping.complete(),
---     ["<C-e>"] = cmp.mapping.close(),
---     ["<C-y>"] = cmp.mapping.confirm({
---       behavior = cmp.ConfirmBehavior.Replace,
---       select = false,
---     }),
---     ["<Tab>"] = cmp.mapping(function(fallback)
---       if luasnip.expand_or_jumpable() then
---         luasnip.expand_or_jump()
---       else
---         fallback()
---       end
---     end, { 'i', 's' }),
---     ["<S-Tab>"] = cmp.mapping(function(fallback)
---       if luasnip.jumpable(-1) then
---         luasnip.jump(-1)
---       else
---         fallback()
---       end
---     end, { 'i', 's' }),
---   }),
---   sources = cmp.config.sources({
---     { name = "luasnip" },
---     { name = "nvim_lsp" },
---     { name = "nvim_lsp_signature_help" },
---   }),
--- })
+cmp.setup({
+  -- window = {
+  --   completion = {
+  --     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
+  --   },
+  --   documentation = {
+  --     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
+  --   }
+  -- },
+  formatting = { -- This is for lspkind
+    format = lspkind.cmp_format({
+      mode = 'symbol_text',
+      maxwidth = 50,
+    })
+  },
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<C-y>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = false,
+    }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = cmp.config.sources({
+    { name = "luasnip" },
+    { name = "nvim_lsp" },
+    { name = "nvim_lsp_signature_help" },
+  }),
+})
