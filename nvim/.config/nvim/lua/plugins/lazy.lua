b0874042df72a6ddff361d0d0e1736594476c13f
Phandal <bfeld.111@gmail.com>
2025-01-28 10:53:06 -0600

    updated plugins/removed uneeded/added config lspkind

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -12,28 +12,24 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  'folke/lazy.nvim',                                 -- Plugin Manager
-  'projekt0n/github-nvim-theme',                     -- Github Theme
-  'shaunsingh/nord.nvim',                            -- Nord Theme
-  'ellisonleao/gruvbox.nvim',                        -- Gruvbox Lua Theme
-  'https://github.com/sainnhe/everforest',           -- Everforest Lua Theme
+  'folke/lazy.nvim',                           -- Plugin Manager
+  'projekt0n/github-nvim-theme',               -- Github Theme
+  'shaunsingh/nord.nvim',                      -- Nord Theme
+  'ellisonleao/gruvbox.nvim',                  -- Gruvbox Lua Theme
+  'https://github.com/sainnhe/everforest',     -- Everforest Lua Theme
+  'nvim-tree/nvim-web-devicons',               -- Pretty Dev Icons
+  'j-hui/fidget.nvim',                         -- LSP Server Progress
   {
-    'nvim-tree/nvim-tree.lua',                       -- Tree like file manager
-    dependencies = { 'nvim-tree/nvim-web-devicons' } -- Pretty icons in NvimTree
-  },
-  'hoob3rt/lualine.nvim',                            -- Different modeline
-  'j-hui/fidget.nvim',                               -- LSP Server Progress
-  {
-    'nvim-telescope/telescope.nvim',                 -- Fuzzy Finder
-    dependencies = { 'nvim-lua/plenary.nvim' }       -- Dependency Plugin
+    'nvim-telescope/telescope.nvim',           -- Fuzzy Finder
+    dependencies = { 'nvim-lua/plenary.nvim' } -- Dependency Plugin
   },
   {
-    'nvim-telescope/telescope-fzf-native.nvim',
+    'nvim-telescope/telescope-fzf-native.nvim', -- Native faster fzf for telescope
     build = 'make'
   },
-  'lewis6991/gitsigns.nvim',                         -- Git integration
+  'lewis6991/gitsigns.nvim',           -- Git integration
   {
-    'nvim-treesitter/nvim-treesitter',               -- Syntax highlighting using TreeSitter
+    'nvim-treesitter/nvim-treesitter', -- Syntax highlighting using TreeSitter
     build = ':TSUpdate'
   },
   'windwp/nvim-autopairs',               -- Automatically adds closing bracket
@@ -42,17 +38,10 @@ require('lazy').setup({
   'neovim/nvim-lspconfig',               -- Base set of LSP configs
   'folke/neodev.nvim',                   -- Add functionality to lua server
   {
-    'hrsh7th/nvim-cmp',                  -- Completion engine
-    dependencies = { 'L3MON4D3/LuaSnip' }
-  },
-  'hrsh7th/cmp-nvim-lsp',                -- Completion for LSP
-  'hrsh7th/cmp-nvim-lsp-signature-help', -- Completion Plugin to show signatures
-  {
     'L3MON4D3/LuaSnip',                  -- Snippet Plugin
     lazy = false,
     build = 'make install_jsregexp',
-    dependencies = { 'saadparwaiz1/cmp_luasnip' } -- Completion Plugin to show snippets
   },
-  'nanotee/luv-vimdocs',                          -- libuv docs in Neovim
-  { dir = '~/Development/suitecloud.nvim/main' }  -- Custom Work in progress
+  'nanotee/luv-vimdocs',                         -- libuv docs in Neovim
+  { dir = '~/Development/suitecloud.nvim/main' } -- Custom Work in progress
 })
