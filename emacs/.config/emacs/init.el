1653eb432ca69cf959ca114c41ac5d8bed5805a2
Phandal <bfeld.111@gmail.com>
2025-04-25 14:42:43 -0500

    updated emacs config to add 2 packages to make it a bit more useful

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -3,7 +3,32 @@
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
 (setq visible-bell t)
-
+(setq-default truncate-lines t)
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
+(setq major-mode-remap-alist
+      '((typescript-mode . typescript-ts-mode)
+        (javascript-mode . typescript-ts-mode)
+        (js-json-mode . json-ts-mode)
+        (mhtml-mode . html-ts-mode)
+        (conf-toml-mode . toml-ts-mode)))
 (pixel-scroll-precision-mode 1)
 (which-key-mode 1)
+(ido-mode 1)
 (load-theme 'modus-operandi-tinted)
+(package-install 'corfu)
+(setq corfu-auto t)
+(add-hook 'eglot-managed-mode-hook 'corfu-mode)
+(package-install 'exec-path-from-shell)
+(exec-path-from-shell-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
